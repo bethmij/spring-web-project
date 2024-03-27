5dee00094b697236e0b7e8498d36d3cc8c6c595e
bethmij <bethmij@gmail.com>
2024-03-27 11:41:12 +0530

    customer updated

--- src/main/java/lk/ijse/gdse66/api/CustomerController.java
+++ src/main/java/lk/ijse/gdse66/api/CustomerController.java
@@ -8,7 +8,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 
 @RestController
-@RequestMapping("/api/v1/customersss")
+@RequestMapping("/api/v1/customersssss")
 @CrossOrigin
 public class CustomerController {
 
